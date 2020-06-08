a86742cb793e85699855c6fd52fd23ce08123472
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-08 22:23:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,37 @@
 def create_an_empty_array
-  []
+[]
 end
 
 def create_an_array
-  ["i", "am", "really", "learning"]
+["1","2","3","4"]
 end
 
 def add_element_to_end_of_array(array, element)
-  ["I", "am", "really", "learning"].push("arrays!")
+array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  ["I", "am", "really", "learning","arrays!"].unshift("wow")
+array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-   ["wow", "I", "am", "really", "learning","arrays!"].pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-   ["wow", "I", "am", "really", "learning"].shift
+  array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  ["I", "am", "really", "learning"][1]
+def retrieve_element_from_index(array, index_number=2)
+array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  ["wow","I", "am", "really", "learning"][0]
+array[0]
 end
 
 def retrieve_last_element_from_array(array)
-    ["I", "am", "really", "learning","arrays!"][-1]
+  array.reverse[0]
+  #array[-1]
+  #array.last
 end
